14f6b98ca22e4bde8670b6938db8396a9f29966f
Tehillah Leigh Lapura <leigh@Tehillahs-MacBook-Pro.local>
2021-07-21 17:27:08 +0800

    Leighs experiment

--- src/Frame.js
+++ src/Frame.js
@@ -1,27 +1,80 @@
-import React from "react";
-import ObjectOne from "./Object1";
-import ObjectTwo from "./Object2";
-import ObjectThree from "./Object3";
-import ObjectFour from "./Object4";
+import React, { useState } from "react";
 import styled from "styled-components";
 
+import { styleConfig } from './styleConfig';
+
 function Frame() {
+  const [activeObject, setActiveObject] = useState(undefined);
+
+  const sidebarDisplay = activeObject && (
+    <>
+      <p>Object #{activeObject}</p>
+      <p>{styleConfig[activeObject]}</p>
+    </>
+  );
+
   return (
     <>
       <TopFrame />
       <Container>
         <LeftSideBar />
-        <ObjectOne />
-        <ObjectTwo />
-        <ObjectThree />
-        <ObjectFour />
-        <RightSideBar />
+        <ObjectOne onClick = {() => {setActiveObject(1)}} />
+        <ObjectTwo onClick = {() => {setActiveObject(2)}}/>
+        <ObjectThree onClick = {() => {setActiveObject(3)}} />
+        <ObjectFour onClick = {() => {setActiveObject(4)}} />
+        <RightSideBar>{sidebarDisplay}</RightSideBar>
       </Container>
     </>
   );
 }
 
 export default Frame;
+
+const ObjectOne = styled.div`
+  position: absolute;
+  width: 321px;
+  height: 213px;
+  left: 284px;
+  top: 108px;
+  background-color: #c4c4c4;
+`;
+
+const ObjectTwo = styled.div`
+  position: absolute;
+  width: 137.1px;
+  height: 213px;
+  left: 773.13px;
+  top: 103px;
+  background-color: #c92929;
+  border: 5px solid #000000;
+  box-sizing: border-box;
+  transform: rotate(15deg);
+`;
+
+const ObjectThree = styled.div`
+  position: absolute;
+  width: 321px;
+  height: 213px;
+  left: 287px;
+  top: 439px;
+
+  background: #000000;
+  border: 1px solid #000000;
+  box-sizing: border-box;
+  box-shadow: 10px 25px 5px rgba(0, 0, 0, 0.25);
+`;
+
+const ObjectFour = styled.div`
+  position: absolute;
+  width: 157.39px;
+  height: 213px;
+  left: 859.61px;
+  top: 430px;
+
+  background: #36c929;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  transform: rotate(45deg);
+`;
 
 const TopFrame = styled.div`
   width: 100%;
@@ -41,6 +94,8 @@ const LeftSideBar = styled.div`
 `;
 
 const RightSideBar = styled.div`
+  padding: 0 10px;
+  display: flex;
   width: 220px;
   height: 100vh;
   border: 2px solid #000000;
---
+++ src/styleConfig.js
@@ -0,0 +1,34 @@
+export const styleConfig = {
+  1: `position: absolute;\n
+      width: 321px;\n
+      height: 213px;\n
+      left: 284px;\n
+      top: 108px;\n
+      background-color: #c4c4c4;`,
+  2: `position: absolute;\n
+      width: 137.1px;\n
+      height: 213px;\n
+      left: 773.13px;\n
+      top: 103px;\n
+      background-color: #c92929;\n
+      border: 5px solid #000000;\n
+      box-sizing: border-box;\n
+      transform: rotate(15deg);`,
+  3: `position: absolute;\n
+      width: 321px;\n
+      height: 213px;\n
+      left: 287px;\n
+      top: 439px;\n
+      background: #000000;\n
+      border: 1px solid #000000;\n
+      box-sizing: border-box;\n
+      box-shadow: 10px 25px 5px rgba(0, 0, 0, 0.25);`,
+  4: `position: absolute;\n
+      width: 157.39px;\n
+      height: 213px;\n
+      left: 859.61px;\n
+      top: 430px;\n
+      background: #36c929;\n
+      box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);\n
+      transform: rotate(45deg);`,
+}
